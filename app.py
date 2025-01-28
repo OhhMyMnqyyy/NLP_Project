2fff17f30ab81dddf23e802e525ef8ef625dd2a6
AimanRifqy <93848455+OhhMyMnqyyy@users.noreply.github.com>
2025-01-28 19:40:22 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 import streamlit as st
-import pandas as pd
-from sklearn.model_selection import train_test_split
-from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.naive_bayes import MultinomialNB
-from sklearn.metrics import accuracy_score
+#import pandas as pd
+#from sklearn.model_selection import train_test_split
+#from sklearn.feature_extraction.text import CountVectorizer
+#from sklearn.naive_bayes import MultinomialNB
+#from sklearn.metrics import accuracy_score
 
 # Streamlit app title
 st.title("Sentiment Analysis for Customer Reviews")
